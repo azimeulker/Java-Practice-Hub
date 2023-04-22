55aa0153d1570ae3a52c769db3a608cd40372c1d
Azime Ulker <115192426+azimeulker@users.noreply.github.com>
2023-04-22 13:23:20 -0500

    Add files via upload

---
+++ circumference/Circumference.java
@@ -0,0 +1,26 @@
+/*
+ * Name: Azime Ulker
+ *Program: Business Information Technology
+ *Course: ADEV-1008 Programming 1
+ *Created:2022-10-22
+ */
+
+ /**
+  *This program calculates circumference of a circle.
+  *
+  *@author Azime Ulker
+  *@version 1.0
+  */
+public class Circumference
+ {
+     public static void main(String[] args)
+     {
+     	double radius = 3.2;
+
+     	final double PI = 3.14;
+
+     	double calculation = 2 * PI * radius;
+
+		System.out.println("Circumference of a circle is " + calculation + ".");
+     }
+ }
