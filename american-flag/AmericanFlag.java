69d7b816ba4a40e70242159f6aec881785e598cf
Azime Ulker <115192426+azimeulker@users.noreply.github.com>
2023-04-22 13:20:16 -0500

    Add files via upload

---
+++ american-flag/AmericanFlag.java
@@ -0,0 +1,34 @@
+/*
+ * Name: Azime Ulker
+ *Program: Business Information Technology
+ *Course: ADEV-1008 Programming 1
+ *Created:2022-11-27
+ */
+
+ /**
+  *This program prints an American flag.
+  *
+  *@author Azime Ulker
+  *@version 1.0
+  */
+ public class AmericanFlag
+{
+   public static void main(String[] args)
+   {
+   	  System.out.println("  * * * * * * ==================================");
+   	  System.out.println("   * * * * *  ==================================");
+   	  System.out.println("  * * * * * * ==================================");
+   	  System.out.println("   * * * * *  ==================================");
+   	  System.out.println("  * * * * * * ==================================");
+   	  System.out.println("   * * * * *  ==================================");
+   	  System.out.println("  * * * * * * ==================================");
+   	  System.out.println("   * * * * *  ==================================");
+   	  System.out.println("  * * * * * * ==================================");
+   	  System.out.println("  ==============================================");
+   	  System.out.println("  ==============================================");
+   	  System.out.println("  ==============================================");
+   	  System.out.println("  ==============================================");
+   	  System.out.println("  ==============================================");
+   	  System.out.println("  ==============================================");
+   }
+}
