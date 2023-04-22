b79138ea107bef6735361fe41a62d08ddfb8c5d3
Azime Ulker <115192426+azimeulker@users.noreply.github.com>
2023-04-22 13:28:46 -0500

    Add files via upload

---
+++ multiplication/Multiplication.java
@@ -0,0 +1,28 @@
+/*
+* Name: Azime Ulker
+*Program: Business Information Technology
+*Course: ADEV-1008 Programming 1
+*Created:2022-11-21
+*/
+
+/**
+*This program takes a number as input and prints
+*its multiplication table upto 10.
+*
+*@author Azime Ulker
+*@version 1.0
+*/
+
+public class Multiplication
+{
+    public static void main(String[] args)
+    {
+    	int a = 8;
+
+    	for(int i = 1; i <= 10; i++)
+    	{
+			System.out.println(a +" * " + i + " = " + a * i );
+		}
+    }
+
+}
