753ad148a49ac8935c41c7c67d38dee6d650da4f
Azime Ulker <115192426+azimeulker@users.noreply.github.com>
2023-04-22 13:24:28 -0500

    Add files via upload

---
+++ computing-expressions/ComputingExpressions.java
@@ -0,0 +1,22 @@
+ /*
+ * Name: Azime Ulker
+ *Program: Business Information Technology
+ *Course: ADEV-1008 Programming 1
+ *Created:2022-11-27
+ */
+
+ /**
+  *This program computes the specified expressions
+  *and print the output
+  *
+  *@author Azime Ulker
+  *@version 1.0
+  */
+ public class ComputingExpressions
+{
+   public static void main(String[] args)
+   {
+		double calculation = (25.5*3.5-3.5*3.5)/(40.5-4.5);
+		System.out.println(calculation);
+   }
+ }
