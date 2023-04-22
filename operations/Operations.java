2234c1311c501535782e28a3a1212ec40ba01e61
Azime Ulker <115192426+azimeulker@users.noreply.github.com>
2023-04-22 13:31:02 -0500

    Add files via upload

---
+++ operations/Operations.java
@@ -0,0 +1,24 @@
+/*
+* Name: Azime Ulker
+*Program: Business Information Technology
+*Course: ADEV-1008 Programming 1
+*Created:2022-11-21
+*/
+
+/**
+*This prints the results of the following operations
+*
+*@author Azime Ulker
+*@version 1.0
+*/
+
+ public class Operations
+ {
+     public static void main(String[] args)
+     {
+		System.out.println(-5 + 8 * 6);
+		System.out.println((55+9) % 9);
+		System.out.println(20 + -3*5 / 8);
+		System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+     }
+ }
