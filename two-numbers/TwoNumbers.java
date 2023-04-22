61f6eba17ad885d122b538471c1bb88a3dbf75e3
Azime Ulker <115192426+azimeulker@users.noreply.github.com>
2023-04-22 13:35:57 -0500

    Add files via upload

---
+++ two-numbers/TwoNumbers.java
@@ -0,0 +1,29 @@
+/*
+* Name: Azime Ulker
+*Program: Business Information Technology
+*Course: ADEV-1008 Programming 1
+*Created:2022-11-21
+*/
+
+/**
+*This program prints the sum, multiply, subtract, divide,
+* and remainder of two numbers.
+*
+*@author Azime Ulker
+*@version 1.0
+*/
+
+public class TwoNumbers
+{
+    public static void main(String[] args)
+    {
+    	int firstNum = 125;
+    	int secondNum = 24;
+
+    	System.out.println(firstNum + secondNum);
+    	System.out.println(firstNum - secondNum);
+    	System.out.println(firstNum * secondNum);
+    	System.out.println(firstNum / secondNum);
+    	System.out.println(firstNum % secondNum);
+    }
+}
